4a4a3ec2567e4e73101789b8b58d155349989ab5
Senya Volkov <formobileprima@gmail.com>
2020-02-22 13:58:52 +0200

    Month start

--- src/components/month/Month.js
+++ src/components/month/Month.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
 export class Month extends React.PureComponent {
+  // generateMonth = (year, month) => {
+  //   const currentDate = new Date(year, month);
+  //   const nextDate = new Date(year, month + 1);
+  //
+  //   let startDay = currentDate.getDay() ? currentDate.getDay() : 7; // ?
+  //
+  //   nextDate.setDate(-0);
+  //
+  //   const days = nextDate.getDate();
+  //
+  //   const tdList = [];
+  //
+  //   for (let i = 1; i <= 7; i += 1) {
+  //     tdList.push((
+  //       <tr>It is TR</tr>
+  //     ));
+  //   }
+  //
+  //   // console.log(currentDate, nextDate, days);
+  //   return tdList;
+  // };
+
   render() {
     return (
-      <p>Month</p>
+      <table>
+        <thead>
+          <tr>
+            <th>ПН</th>
+            <th>ВТ</th>
+            <th>СР</th>
+            <th>ЧТ</th>
+            <th>ПТ</th>
+            <th>СБ</th>
+            <th>НД</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            this.generateMonth(2020, 0)
+          }
+        </tbody>
+      </table>
     );
   }
 }
