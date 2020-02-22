41485db864cbf2ed1e6b08301efae4c0f4b30e1d
Senya Volkov <formobileprima@gmail.com>
2020-02-22 13:06:36 +0200

    upgrade basic logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
 import React from 'react';
 import './App.scss';
+import { Month } from './components/month/Month';
 
-export const App = () => (
-  <div className="app">
-    <h1 className>Mate Hackaton</h1>
-    <button type="button" className="btn btn-primary">Start</button>
-  </div>
-);
+const initialTasks = {
+  2020: {
+
+    jan: {
+      1: {
+        name: 'name of task jan-2020-1',
+        value: 'value of task jan-2020-1',
+      },
+      2: {
+        name: 'name of task jan-2020-2',
+        value: 'value of task jan-2020-2',
+      },
+    },
+
+    feb: {
+      1: {
+        name: 'name of task feb-2020-1',
+        value: 'value of task feb-2020-1',
+      },
+      2: {
+        name: 'name of task feb-2020-2',
+        value: 'value of task feb-2020-2',
+      },
+    },
+
+  },
+
+  2018: {
+
+    jan: {
+      1: {
+        name: 'name of task jan-2018-1',
+        value: 'value of task jan-2018-1',
+      },
+      2: {
+        name: 'name of task jan-2018-2',
+        value: 'value of task jan-2018-2',
+      },
+    },
+
+    feb: {
+      1: {
+        name: 'name of task feb-2018-1',
+        value: 'value of task feb-2018-1',
+      },
+      2: {
+        name: 'name of task feb-2018-2',
+        value: 'value of task feb-2018-2',
+      },
+    },
+
+  },
+};
+
+export class App extends React.Component {
+  state = {
+    listTasks: initialTasks,
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Mate Hackaton</h1>
+        <Month listTasks={this.state.listTasks} />
+      </div>
+    );
+  }
+}
---
+++ src/components/month/Month.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class Month extends React.PureComponent {
+  render() {
+    return (
+      <p>Month</p>
+    );
+  }
+}
