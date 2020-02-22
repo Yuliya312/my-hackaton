e678fa98c902539c25a09179c8527309be317dce
Senya Volkov <formobileprima@gmail.com>
2020-02-22 16:19:59 +0200

    Month start upgrade2

--- src/App.js
+++ src/App.js
@@ -6,25 +6,33 @@ const initialTasks = {
   2020: {
 
     jan: {
-      1: {
-        name: 'name of task jan-2020-1',
-        value: 'value of task jan-2020-1',
-      },
-      2: {
-        name: 'name of task jan-2020-2',
-        value: 'value of task jan-2020-2',
-      },
+      1: [
+        {
+          name: 'name of task jan-2020-1',
+          value: 'value of task jan-2020-1',
+        },
+      ],
+      5: [
+        {
+          name: 'name of task jan-2020-2',
+          value: 'value of task jan-2020-2',
+        },
+      ],
     },
 
     feb: {
-      1: {
-        name: 'name of task feb-2020-1',
-        value: 'value of task feb-2020-1',
-      },
-      2: {
-        name: 'name of task feb-2020-2',
-        value: 'value of task feb-2020-2',
-      },
+      1: [
+        {
+          name: 'name of task feb-2020-1',
+          value: 'value of task feb-2020-1',
+        },
+      ],
+      2: [
+        {
+          name: 'name of task feb-2020-2',
+          value: 'value of task feb-2020-2',
+        },
+      ],
     },
 
   },
@@ -32,25 +40,33 @@ const initialTasks = {
   2018: {
 
     jan: {
-      1: {
-        name: 'name of task jan-2018-1',
-        value: 'value of task jan-2018-1',
-      },
-      2: {
-        name: 'name of task jan-2018-2',
-        value: 'value of task jan-2018-2',
-      },
+      1: [
+        {
+          name: 'name of task jan-2018-1',
+          value: 'value of task jan-2018-1',
+        },
+      ],
+      2: [
+        {
+          name: 'name of task jan-2018-2',
+          value: 'value of task jan-2018-2',
+        },
+      ],
     },
 
     feb: {
-      1: {
-        name: 'name of task feb-2018-1',
-        value: 'value of task feb-2018-1',
-      },
-      2: {
-        name: 'name of task feb-2018-2',
-        value: 'value of task feb-2018-2',
-      },
+      1: [
+        {
+          name: 'name of task feb-2018-1',
+          value: 'value of task feb-2018-1',
+        },
+      ],
+      2: [
+        {
+          name: 'name of task feb-2018-2',
+          value: 'value of task feb-2018-2',
+        },
+      ],
     },
 
   },
--- src/components/month/Month.js
+++ src/components/month/Month.js
@@ -1,29 +1,63 @@
 import React from 'react';
+import './Month.scss';
+import PropTypes from 'prop-types';
 
 export class Month extends React.PureComponent {
-  // generateMonth = (year, month) => {
-  //   const currentDate = new Date(year, month);
-  //   const nextDate = new Date(year, month + 1);
-  //
-  //   let startDay = currentDate.getDay() ? currentDate.getDay() : 7; // ?
-  //
-  //   nextDate.setDate(-0);
-  //
-  //   const days = nextDate.getDate();
-  //
-  //   const tdList = [];
-  //
-  //   for (let i = 1; i <= 7; i += 1) {
-  //     tdList.push((
-  //       <tr>It is TR</tr>
-  //     ));
-  //   }
-  //
-  //   // console.log(currentDate, nextDate, days);
-  //   return tdList;
-  // };
+  generateMonth = (year, month) => {
+    const currentDate = new Date(year, month);
+    const nextDate = new Date(year, month + 1);
+    const startDay = currentDate.getDay() ? currentDate.getDay() : 7; // ?
+
+    nextDate.setDate(-0);
+    const days = nextDate.getDate();
+
+    return {
+      currentDate,
+      nextDate,
+      startDay,
+      days,
+    };
+  };
 
   render() {
+    const { listTasks } = this.props;
+    // console.log(listTasks[2018])
+
+    const canlendarBasicInform = this.generateMonth(2020, 0);
+    const blanks = [];
+
+    for (let i = 1; i < canlendarBasicInform.startDay; i += 1) {
+      blanks.push((
+        ''
+      ));
+    }
+
+    const filled = [];
+
+    for (let i = 1; i <= canlendarBasicInform.days; i += 1) {
+      filled.push((
+        i
+      ));
+    }
+
+    const totalSorst = [...blanks, ...filled];
+
+    const list = [];
+
+    totalSorst.forEach((day, index) => {
+      if (index % 7 === 0) {
+        const trObj = [];
+
+        for (let i = 0; i < 7; i += 1) {
+          trObj[i] = totalSorst[index + i];
+        }
+
+        list.push(trObj);
+      }
+    });
+
+    // console.log(list)
+
     return (
       <table>
         <thead>
@@ -40,10 +74,35 @@ export class Month extends React.PureComponent {
 
         <tbody>
           {
-            this.generateMonth(2020, 0)
+            list.map((trItem, index) => {
+              return (
+                <tr>
+                  {
+                    trItem.map((tdItem) => {
+                      return (
+                        <td>
+                          {tdItem}
+                          { listTasks[2020].jan[tdItem]
+                            ? listTasks[2020].jan[tdItem]
+                              .map(item => (
+                                <div key={item.name}>item.value</div>
+                              ))
+                            : null
+                          }
+                        </td>
+                      );
+                    })
+                  }
+                </tr>
+              );
+            })
           }
         </tbody>
       </table>
     );
   }
 }
+
+Month.propTypes = {
+  listTasks: PropTypes.shape({}).isRequired,
+};
